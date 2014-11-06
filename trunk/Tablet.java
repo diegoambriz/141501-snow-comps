585414047306f57da9ccdd0f7ff165aadffce4d3
diego.ambriz93@gmail.com <diego.ambriz93@gmail.com@665482b8-64d9-4b7b-9238-573d4d59c416>
2014-11-06 09:18:22 +0000

    Creacion de las clases del Proyecto

---
+++ trunk/Tablet.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Tablet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tablet extends Bonus
+{
+    /**
+     * Act - do whatever the Tablet wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
