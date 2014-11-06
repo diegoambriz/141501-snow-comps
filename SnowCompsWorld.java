c74d88bec95dc5e1b083f4b9a7ea6b8443a4b397
diego.ambriz93 <diego.ambriz93@gmail.com>
2014-11-06 09:18:22 +0000

    Creacion de las clases del Proyecto

---
+++ SnowCompsWorld.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/** 
+ * @author Diego Alfonso Ambriz Martinez 
+ * @version 05-11-2014
+ */
+public class SnowCompsWorld extends World
+{
+
+    /**
+     * Constructor for objects of class SnowCompsWorld.
+     * 
+     */
+    public SnowCompsWorld()
+    {    
+        // Create a new world with 800x600 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+    }
+}
