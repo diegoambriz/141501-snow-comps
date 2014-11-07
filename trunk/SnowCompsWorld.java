e9fee421bcf4ce6663c837eae375a1c88bd5344f
diego.ambriz93@gmail.com <diego.ambriz93@gmail.com@665482b8-64d9-4b7b-9238-573d4d59c416>
2014-11-07 16:41:21 +0000

    Se agregaron las plataformas del escenario,y el movimiento a la izquierda y a la derecha del personaje

--- trunk/SnowCompsWorld.java
+++ trunk/SnowCompsWorld.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class SnowCompsWorld extends World
 {
-
+    public static final int TAM_BLOCK=30;
     /**
      * Constructor for objects of class SnowCompsWorld.
      * 
@@ -14,6 +14,29 @@ public class SnowCompsWorld extends World
     public SnowCompsWorld()
     {    
         // Create a new world with 800x600 cells with a cell size of 1x1 pixels.
-        super(800, 600, 1); 
+        super(800, 600, 1);
+        
+        Player player1;
+        player1=new Player();
+        
+        addObject(player1,100,580);
+        
+        Plattaform1 plattaform;
+        plattaform=new Plattaform1();
+        
+        Plattaform1 plattaform3;
+        plattaform3=new Plattaform1();
+        
+        Plattaform2 plattaform2;
+        plattaform2=new Plattaform2();
+        
+        Plattaform2 plattaform4;
+        plattaform4=new Plattaform2();
+        
+        //addObject(plattaform,100,450);
+        addObject(plattaform,130,530);
+        addObject(plattaform2,130,380);
+        addObject(plattaform3,130,230);
+        addObject(plattaform4,130,80);
     }
 }
