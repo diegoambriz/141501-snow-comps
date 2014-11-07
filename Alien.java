98c1c68e4fab1d5a632669997f6111d1fefe72d2
diego.ambriz93 <diego.ambriz93@gmail.com>
2014-11-07 22:46:24 +0000

    Se agregaron todos los sprites del personaje principal

--- Alien.java
+++ Alien.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Alien extends Enemie
+public class Alien extends Enemy
 {
     /**
      * Act - do whatever the Alien wants to do. This method is called whenever
